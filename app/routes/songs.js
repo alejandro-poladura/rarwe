c1a025f8bfb98345cd190d5fc214ba1f80da1698
Alejandro Poladura <alejandro@mimiquate.com>
2023-01-31 14:10:25 -0300

    Linked band name to songs list for the band

--- app/routes/songs.js
+++ app/routes/songs.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
-
-export default class SongsRoute extends Route {}
+export default class BandsBandSongsRoute extends Route {
+  model() {
+    let band = this.modelFor('bands.band');
+    return band.songs;
+  }
+}
