b87784b0e4112642cd0bb8b52f0cd83a2ecb3809
Alejandro Poladura <alejandro@mimiquate.com>
2023-02-01 11:36:21 -0300

    Fixed adding songs to the catalog

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -9,7 +9,7 @@ export class Band {
   constructor({ id, name, songs }) {
     this.id = id;
     this.name = name;
-    this.songs = songs;
+    this.songs = songs || [];
   }
 }
 
@@ -23,45 +23,60 @@ export class Song {
 
 export default class BandsRoute extends Route {
   @service catalog;
+
   model() {
     let blackDog = new Song({
       title: 'Black Dog',
-      band: 'Led Zeppelin',
       rating: 3,
     });
+
     let yellowLedbetter = new Song({
       title: 'Yellow Ledbetter',
-      band: 'Pearl Jam',
       rating: 4,
     });
+
     let pretender = new Song({
       title: 'The Pretender',
-      band: 'Foo Fighters',
       rating: 2,
     });
+
     let daughter = new Song({
       title: 'Daughter',
-      band: 'Pearl Jam',
       rating: 5,
     });
+
     let ledZeppelin = new Band({
       id: 'led-zeppelin',
       name: 'Led Zeppelin',
       songs: [blackDog],
     });
+
     let pearlJam = new Band({
       id: 'pearl-jam',
       name: 'Pearl Jam',
       songs: [yellowLedbetter, daughter],
     });
+
     let fooFighters = new Band({
       id: 'foo-fighters',
       name: 'Foo Fighters',
       songs: [pretender],
     });
+
+    blackDog.band = ledZeppelin;
+    yellowLedbetter.band = pearlJam;
+    daughter.band = pearlJam;
+    pretender.band = fooFighters;
+
+    this.catalog.add('song', blackDog);
+    this.catalog.add('song', yellowLedbetter);
+    this.catalog.add('song', daughter);
+    this.catalog.add('song', pretender);
+
     this.catalog.add('band', ledZeppelin);
     this.catalog.add('band', pearlJam);
     this.catalog.add('band', fooFighters);
+
     return this.catalog.bands;
   }
 }
