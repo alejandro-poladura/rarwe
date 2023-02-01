235379217ad3e2289ac2046c6965b763acdfb15e
Alejandro Poladura <alejandro@mimiquate.com>
2023-02-01 15:37:31 -0300

    Replaced the loading and displaying of bands

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -1,22 +1,40 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { dasherize } from '@ember/string';
 import { tracked } from '@glimmer/tracking';
 import Band from 'rarwe/models/band';
 import { inject as service } from '@ember/service';
+import fetch from 'fetch';
 
 export default class BandsNewController extends Controller {
   @service catalog;
   @service router;
   @tracked name;
+  
   @action
   updateName(event) {
     this.name = event.target.value;
   }
+
   @action
-  saveBand() {
-    let band = new Band({ name: this.name, id: dasherize(this.name) });
-    this.catalog.add('band', band);
-    this.router.transitionTo('bands.band.songs', band.id);
+  async saveBand() {
+    let response = await fetch('/bands', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+      },
+      body: JSON.stringify({
+        data: {
+          type: 'bands',
+          attributes: {
+            name: this.name,
+          },
+        },
+      }),
+    });
+    let json = await response.json();
+    let { id, attributes } = json.data;
+    let record = new Band({ id, ...attributes });
+    this.catalog.add('band', record);
+    this.router.transitionTo('bands.band.songs', id);
   }
 }
