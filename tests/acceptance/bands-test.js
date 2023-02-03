596aa8394f6f3d6213a5b4a042a0eb550f50c627
Alejandro Poladura <alejandro@mimiquate.com>
2023-02-03 11:47:49 -0300

    Fixed band-test.js

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,13 +1,12 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
-import { setupApplicationTest } from 'rarwe/tests/helpers';
+import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | bands', function (hooks) {
   setupApplicationTest(hooks);
 
   test('visiting /bands', async function (assert) {
-    await visit('/bands');
-
-    assert.strictEqual(currentURL(), '/bands');
+    await visit('/');
+    assert.strictEqual(currentURL(), '/');
   });
 });
